eae4a65427452c122298328e642b0c27f1a4ba96
B-Rabbit <binitsaha91@gmail.com>
2026-02-10 00:08:31 +0530

    fix vercel output directory

--- vite.config.ts
+++ vite.config.ts
@@ -22,7 +22,7 @@ export default defineConfig(async () => {
     plugins,
     root: "client",
     build: {
-      outDir: "client/dist",
+      outDir: "../dist",
       emptyOutDir: true,
     },
     resolve: {
